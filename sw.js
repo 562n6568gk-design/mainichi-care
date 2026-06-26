fe1520501f30dcf202eabc7199ca6707a53ab0a6
562n6568gk-design <562n6568gk@privaterelay.appleid.com>
2026-06-26 21:26:26 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="mainichi-aligner-v1";const ASSETS=["./","./index.html","./style.css","./script.js","./manifest.json","./icon.svg"];self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(res=>res||fetch(event.request)))})
